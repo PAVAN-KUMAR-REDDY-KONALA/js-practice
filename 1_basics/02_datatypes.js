1246c147976b8c3cfede1d46da5d1c125a8d39e1
PAVAN-KUMAR-REDDY-KONALA <162803809+PAVAN-KUMAR-REDDY-KONALA@users.noreply.github.com>
2024-06-27 13:17:41 +0000

    variables

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict" //this statement is optional but good to use to treat all the JS code as newer version
+// alert(3+3) ->we're using nodejs but not browser so alert() is not allowed in node
+// console.log(3+3) -> this can be used in node
+
+// js documentation : https://tc39.es/ecma262/     but it is difficult to read
